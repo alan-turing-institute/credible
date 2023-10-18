33196ac7560213137fd6081f6d7325493a22a270
pwochner <pwochner@turing.ac.uk>
2023-10-18 14:15:02 +0100

    Use root event date from config for DID chain display.

--- lib/app/pages/chain/widget/chain.dart
+++ lib/app/pages/chain/widget/chain.dart
@@ -1,18 +1,27 @@
+import 'package:credible/app/interop/secure_storage/secure_storage.dart';
 import 'package:credible/app/pages/chain/models/chain.dart';
 import 'package:credible/app/pages/chain/widget/tile.dart';
 import 'package:credible/app/pages/did/widget/document.dart';
+import 'package:credible/app/pages/profile/blocs/config.dart';
+import 'package:credible/app/pages/profile/models/config.dart';
+import 'package:credible/app/pages/profile/models/root.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:intl/intl.dart';
 
 class DIDChainWidgetModel {
   final List<DIDDocumentWidgetModel> data;
+  final String? rootEventDate;
 
-  const DIDChainWidgetModel(this.data);
+  const DIDChainWidgetModel(this.data, this.rootEventDate);
 
-  factory DIDChainWidgetModel.fromDIDChainModel(DIDChainModel model) {
-    return DIDChainWidgetModel(model.didChain
-        .map((m) => DIDDocumentWidgetModel.fromDIDModel(m))
-        .toList());
+  factory DIDChainWidgetModel.fromDIDChainModel(
+      DIDChainModel model, String? rootEventDate) {
+    return DIDChainWidgetModel(
+        model.didChain
+            .map((m) => DIDDocumentWidgetModel.fromDIDModel(m))
+            .toList(),
+        rootEventDate);
   }
 }
 
@@ -35,13 +44,17 @@ class DIDChainWidget extends StatelessWidget {
                 final item = entry.value;
                 final isFirst = index == 0;
                 final isLast = index == model.data.length - 1;
+                final rootEventDatetime =
+                    DateTime.parse(model.rootEventDate ?? '');
 
                 return CustomTile(
                   model: item,
                   isFirst: isFirst,
                   isLast: isLast,
-                  rootEventDate:
-                      DateFormat('dd MMM yyyy', 'en_UK').format(DateTime.now()),
+                  rootEventDate: DateFormat('dd MMM yyyy', 'en_UK')
+                      .format(rootEventDatetime),
+
+                  // DateTime.parse(rootConfigModel),
                 );
               }).toList()
             : [],
