f3129e745a59c33179f34d9a162d2e2d35fa06cf
Tim Hobson <thobson88@gmail.com>
2024-12-02 16:04:57 +0000

    Add disclaimer & key gen display text

--- lib/app/pages/on_boarding/success.dart
+++ lib/app/pages/on_boarding/success.dart
@@ -18,15 +18,15 @@ class OnBoardingSuccessPage extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Flexible(flex: 1, child: Container()),
-          HeroFix(
-            tag: 'splash/icon',
-            child: Container(
-              width: MediaQuery.of(context).size.width * 0.2,
-              height: MediaQuery.of(context).size.width * 0.2,
-              child: SvgPicture.asset('assets/brand/logo-circular.svg'),
-            ),
-          ),
-          const SizedBox(height: 32.0),
+          // HeroFix(
+          //   tag: 'splash/icon',
+          //   child: Container(
+          //     width: MediaQuery.of(context).size.width * 0.2,
+          //     height: MediaQuery.of(context).size.width * 0.2,
+          //     child: SvgPicture.asset('assets/brand/logo-circular.svg'),
+          //   ),
+          // ),
+          // const SizedBox(height: 32.0),
           Center(
             child: Text(
               localizations.onBoardingSuccessTitle,
@@ -34,10 +34,28 @@ class OnBoardingSuccessPage extends StatelessWidget {
               textAlign: TextAlign.center,
             ),
           ),
+          const SizedBox(height: 32.0),
+          Center(
+            child: Text(
+              // TODO: add to localizations.
+              'Your personal decentralized identifier (DID) has been generated.',
+              style: Theme.of(context).textTheme.bodyLarge,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          const SizedBox(height: 48.0),
+          Center(
+            child: Text(
+              // TODO: add to localizations.
+              'You can regenerate the same DID in future using your mnemonic pass phrase.',
+              style: Theme.of(context).textTheme.bodyLarge,
+              textAlign: TextAlign.center,
+            ),
+          ),
           Flexible(flex: 3, child: Container()),
           BaseButton.primary(
             onPressed: () {
-              Modular.to.pushReplacementNamed('/credentials/list');
+              Modular.to.pushNamedAndRemoveUntil('/profile', (p0) => false);
             },
             child: Text(localizations.onBoardingSuccessButton),
           ),
